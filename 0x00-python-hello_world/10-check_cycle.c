fd66b9a69409ccbab6a740e7dfaab163c36a335c
Jukunye <jukunyes@gmail.com>
2023-08-08 05:33:09 +0300

    lists

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include "lists.h"
 
 /**
@@ -6,24 +7,24 @@
  *
  * Return: 1 if the list has a cycle, 0 if it doesn't
  */
-
 int check_cycle(listint_t *list)
 {
 	listint_t *slow, *fast;
 
-	if (!list)
+	if (list == NULL || list->next == NULL)
 		return (0);
 
-	slow = list;
-	fast = list->next;
-	while (fast && slow && fast->next)
+	slow = list->next;
+	fast = list->next->next;
+
+	while (slow && fast && fast->next)
 	{
 		if (slow == fast)
-		{
 			return (1);
-		}
-		slow = slow->next;
+
+		turtle = slow->next;
 		fast = fast->next->next;
 	}
+
 	return (0);
 }
