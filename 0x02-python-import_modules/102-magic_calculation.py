18cccedfbe55a3a2cb8088f1f32e480c9da95660
Jukunye <jukunyes@gmail.com>
2023-08-12 01:49:02 +0300

    100-my_calculator.py

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 def magic_calculation(a, b):
     """Match bytecode provided by Alx School."""
     from magic_calculation_102 import add, sub
@@ -8,7 +7,5 @@ def magic_calculation(a, b):
         c = add(a, b)
         for i in range(4, 6):
             c = add(c, i)
-        return (c)
-
-    else:
-        return(sub(a, b))
+        return c
+    return sub(a, b)
