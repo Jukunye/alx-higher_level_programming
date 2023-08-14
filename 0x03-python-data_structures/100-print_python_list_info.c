9c58a5b5b4ca1f81567bddf4f774524c426da62e
Jukunye <jukunyes@gmail.com>
2023-08-14 22:45:24 +0300

    add tuple

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <Python.h>
 
 /**
@@ -11,13 +12,11 @@
 void print_python_list_info(PyObject *p)
 {
     long int size, i;
-    PyListObject *list;
+    PyListObject *list = (PyListObject *)p;
     PyObject *item;
 
     size = Py_SIZE(p);
     printf("[*] Size of the Python List = %ld\n", size);
-
-    list = (PyListObject *)p;
     printf("[*] Allocated = %ld\n", list->allocated);
 
     for (i = 0; i < size; i++)
