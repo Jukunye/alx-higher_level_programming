4f99f2d86ec9f2d541d414766959600825f5b4a4
Jukunye <jukunyes@gmail.com>
2023-08-28 23:46:23 +0300

    cpython

---
+++ 0x05-python-exceptions/103-python.c
@@ -0,0 +1,64 @@
+#include <Python.h>
+
+/**
+ * print_python_bytes - prints bytes in a python byte object
+ * @p: A Python Object pointer
+ */
+void print_python_bytes(PyObject *p)
+{
+	PyBytesObject *bytes = (PyBytesObject *)p;
+	Py_ssize_t i, size;
+
+	fflush(stdout);
+
+	printf("[.] bytes object info\n");
+	if (!PyBytes_Check(p))
+	{
+		printf("  [ERROR] Invalid Bytes Object\n");
+		return;
+	}
+	size = ((PyVarObject *)(p))->ob_size;
+
+	printf("  size: %ld\n", size);
+	printf("  trying string: %s\n", bytes->ob_sval);
+
+	if (size >= 10)
+		size = 10;
+	else
+		size += 1;
+
+	printf("  first %ld bytes: ", size);
+	for (i = 0; i < size; i++)
+	{
+		printf("%02hhx", bytes->ob_sval[i]);
+		if (i == (size - 1))
+			printf("\n");
+		else
+			printf(" ");
+	}
+}
+
+/**
+ * print_python_float - Prints basic info about Python float objects.
+ * @p: A PyObject float object.
+ */
+void print_python_float(PyObject *p)
+{
+	char *buffer = NULL;
+
+	PyFloatObject *float_obj = (PyFloatObject *)p;
+
+	fflush(stdout);
+
+	printf("[.] float object info\n");
+	if (strcmp(p->ob_type->tp_name, "float") != 0)
+	{
+		printf("  [ERROR] Invalid Float Object\n");
+		return;
+	}
+
+	buffer = PyOS_double_to_string(float_obj->ob_fval, 'r', 0,
+			Py_DTSF_ADD_DOT_0, NULL);
+	printf("  value: %s\n", buffer);
+	PyMem_Free(buffer);
+}
