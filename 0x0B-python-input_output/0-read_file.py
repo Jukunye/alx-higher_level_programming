6313cb3f94119ff8aaab464e36a59ea60b43f60a
Jukunye <jukunyes@gmail.com>
2023-09-12 21:30:51 +0300

    student to disk and reload

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,5 +4,5 @@
 
 def read_file(filename=""):
     """reads a text file (utf-8) and prints it to stdout"""
-    with open(filename, 'r', encoding="utf-8") as file:
-        print(file.read(), end="")
+    with open(filename, "r", encoding="utf-8") as f:
+        print(f.read(), end="")
