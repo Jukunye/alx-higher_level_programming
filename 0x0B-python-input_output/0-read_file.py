60aef8f5f0c3e44b21a383aad05b19e654bb88ac
Jukunye <jukunyes@gmail.com>
2023-09-12 09:49:07 +0300

    write file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""this module defines a function read_file"""
+
+
 def read_file(filename=""):
     """reads a text file (utf-8) and prints it to stdout"""
     with open(filename, 'r', encoding="utf-8") as file:
