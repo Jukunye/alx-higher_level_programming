a616bd735d8b4fe89bb2757ce7820dccfee24376
Jukunye <jukunyes@gmail.com>
2023-09-18 21:12:40 +0300

    test

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,5 +1,6 @@
 import unittest
 from models import base
+from models.base import Base
 
 
 class TestBase(unittest.TestCase):
@@ -78,3 +79,40 @@ class TestBase(unittest.TestCase):
     def test_tuple_ID(self):
         b = base.Base((1, 2, 3))
         self.assertEqual(b.id, (1, 2, 3))
+
+class testBase_to_JSON_string(unittest.TestCase):
+    """
+    Tests for the static method def to_json_string(list_dictionaries):
+    that returns the JSON string representation of list_dictionaries
+    """
+
+    def test_to_JSON_string_docstring(self):
+        self.assertIsNotNone(Base.to_json_string.__doc__)
+
+    def test_empty_list(self):
+        self.assertEqual(Base.to_json_string([]), "[]")
+
+    def test_None_list(self):
+        self.assertEqual(Base.to_json_string(None), "[]")
+
+    def test_single_dictionary(self):
+        input_list = [{"a": 1, "b": 2, "c": 3}]
+        result = '[{"a": 1, "b": 2, "c": 3}]'
+        self.assertEqual(Base.to_json_string(input_list),result)
+
+    def test_multiple_dictionaries(self):
+        input_list = [{"a": 1, "b": 2, "c": 3},{"x": 1, "y": 2},
+                      {"id": 35, "name": "value", "c": 3}]
+        result = '[{"a": 1, "b": 2, "c": 3}, {"x": 1, "y": 2},'\
+            ' {"id": 35, "name": "value", "c": 3}]'
+        self.assertEqual(Base.to_json_string(input_list),result)
+
+    def test_check_class(self):
+        self.assertTrue(isinstance(Base.to_json_string([{"a": 1, "b": 2, "c": 3}]), str))
+
+class testBase_JSON_string_to_file(unittest.TestCase):
+    """
+    Tests for the class method def save_to_file(cls, list_objs):
+    that writes the JSON string representation of list_objs to a file
+    """
+    pass
