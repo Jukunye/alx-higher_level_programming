cb01012231c666992e586e1a6ea2f19d4848f77a
Jukunye <jukunyes@gmail.com>
2023-09-18 21:04:40 +0300

    update base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """this module defines the fundation class Base
 """
+import json
 
 
 class Base:
@@ -14,3 +15,59 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Returns the JSON string representation of list_dictionaries"""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        Save a list of instances to a JSON file.
+
+        Args:
+            cls: The class itself.
+            list_objs (list): A list of instances to save to a file.
+
+        Returns:
+            None
+        """
+        filename = cls.__name__ + ".json"
+        with open(filename, "w") as f:
+            if list_objs is None:
+                f.write("[]")
+            else:
+                list_dicts = [i.to_dictionary() for i in list_objs]
+                f.write(Base.to_json_string(list_dicts))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """
+        Save a list of instances to a JSON file.
+
+        Args:
+            cls: The class itself.
+            list_objs (list): A list of instances to save to a file.
+
+        Returns:
+            None
+        """
+        if json_string is None or json_string == "[]":
+            return []
+        return json.loads(json_string)
+    
+    @classmethod
+    def create(cls, **dictionary):
+        """
+        Returns an instance with all attributes already set
+        """
+        if dictionary and dictionary != {}:
+            if cls.__name__ == "Rectangle":
+                obj = cls(1, 1)
+            else:
+                obj = cls(1)
+            obj.update(**dictionary)
+            return obj
