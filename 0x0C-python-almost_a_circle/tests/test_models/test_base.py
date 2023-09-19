71ce2469ef9f34ee06f8ecbbc54b7dfe6da0ff7b
Jukunye <jukunyes@gmail.com>
2023-09-19 04:50:20 +0300

    update tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,6 +1,9 @@
 import unittest
+import os
 from models import base
 from models.base import Base
+from models.square import Square
+from models.rectangle import Rectangle
 
 
 class TestBase(unittest.TestCase):
@@ -80,6 +83,7 @@ class TestBase(unittest.TestCase):
         b = base.Base((1, 2, 3))
         self.assertEqual(b.id, (1, 2, 3))
 
+
 class testBase_to_JSON_string(unittest.TestCase):
     """
     Tests for the static method def to_json_string(list_dictionaries):
@@ -98,21 +102,151 @@ class testBase_to_JSON_string(unittest.TestCase):
     def test_single_dictionary(self):
         input_list = [{"a": 1, "b": 2, "c": 3}]
         result = '[{"a": 1, "b": 2, "c": 3}]'
-        self.assertEqual(Base.to_json_string(input_list),result)
+        self.assertEqual(Base.to_json_string(input_list), result)
 
     def test_multiple_dictionaries(self):
-        input_list = [{"a": 1, "b": 2, "c": 3},{"x": 1, "y": 2},
+        input_list = [{"a": 1, "b": 2, "c": 3}, {"x": 1, "y": 2},
                       {"id": 35, "name": "value", "c": 3}]
         result = '[{"a": 1, "b": 2, "c": 3}, {"x": 1, "y": 2},'\
             ' {"id": 35, "name": "value", "c": 3}]'
-        self.assertEqual(Base.to_json_string(input_list),result)
+        self.assertEqual(Base.to_json_string(input_list), result)
 
     def test_check_class(self):
-        self.assertTrue(isinstance(Base.to_json_string([{"a": 1, "b": 2, "c": 3}]), str))
+        self.assertTrue(isinstance(
+            Base.to_json_string([{"a": 1, "b": 2, "c": 3}]), str))
 
-class testBase_JSON_string_to_file(unittest.TestCase):
+
+class testBase_save_to_file(unittest.TestCase):
     """
     Tests for the class method def save_to_file(cls, list_objs):
     that writes the JSON string representation of list_objs to a file
+    """
+    def test_save_to_file_docstring(self):
+        self.assertIsNotNone(Base.save_to_file.__doc__)
+
+    def test_save_to_file_Empty(self):
+        instances = []
+        Base.save_to_file(instances)
+        self.assertTrue(os.path.exists("Base.json"))
+
+    def test_save_to_file_None(self):
+        Base.save_to_file(None)
+        self.assertTrue(os.path.exists("Base.json"))
+
+    def test_save_to_file_no_args(self):
+        with self.assertRaises(TypeError):
+            Rectangle.save_to_file()
+
+    def test_save_to_file_rectangle(self):
+        r = Rectangle(1, 2)
+        Rectangle.save_to_file([r])
+        with open("Rectangle.json", "r") as f:
+            self.assertTrue(len(f.read()) == 53)
+
+    def test_save_to_file_square(self):
+        s = Square(10)
+        Square.save_to_file([s])
+        self.assertTrue(os.path.exists("Square.json"))
+
+    def tearDown(self):
+        # Clean up created files after each test
+        if os.path.exists("Rectangle.json"):
+            os.remove("Rectangle.json")
+        if os.path.exists("Base.json"):
+            os.remove("Base.json")
+        if os.path.exists("Square.json"):
+            os.remove("Square.json")
+
+
+class testBase_from_json_string(unittest.TestCase):
+    """
+    Tests for static method def from_json_string(json_string):
+    that returns the list of the JSON string representation json_string
+    """
+
+    def test_from_json_string_docstring(self):
+        self.assertIsNotNone(Base.from_json_string.__doc__)
+
+    def test_from_json_string_no_args(self):
+        with self.assertRaises(TypeError):
+            Base.from_json_string()
+
+    def test_from_json_string_args(self):
+        with self.assertRaises(TypeError):
+            Base.from_json_string([], "hello")
+
+    def test_from_json_string_valid(self):
+        json_string = '[{"id": 1, "name": "John"}, {"id": 2, "name": "Jane"}]'
+        result = Base.from_json_string(json_string)
+        self.assertIsInstance(result, list)
+        expected = [{"id": 1, "name": "John"}, {"id": 2, "name": "Jane"}]
+        self.assertEqual(result, expected)
+
+    def test_from_json_string_None(self):
+        self.assertEqual(Base.from_json_string(None), [])
+
+    def test_from_json_string_empty(self):
+        self.assertEqual(Base.from_json_string("[]"), [])
+
+
+class testBase_create(unittest.TestCase):
+    """
+    Tests for static method def from_json_string(json_string):
+    that returns the list of the JSON string representation json_string
+    """
+
+    def test_create_square_original(self):
+        s = Square(3, 5, 1, 35)
+        s_dictionary = s.to_dictionary()
+        s1 = Square.create(**s_dictionary)
+        self.assertEqual("[Square] (35) 5/1 - 3", str(s))
+
+    def test_create_square_new(self):
+        s = Square(3, id=3)
+        s_dictionary = s.to_dictionary()
+        s1 = Square.create(**s_dictionary)
+        self.assertEqual("[Square] (3) 0/0 - 3", str(s1))
+
+    def test_create_square_is(self):
+        s = Square(2)
+        s_dictionary = s.to_dictionary()
+        s1 = Square.create(**s_dictionary)
+        self.assertIsNot(s, s1)
+
+    def test_create_rectangle_is(self):
+        r1 = Rectangle(3, 5, 1, 2, 7)
+        r1_dictionary = r1.to_dictionary()
+        r2 = Rectangle.create(**r1_dictionary)
+        self.assertIsNot(r1, r2)
+
+    def test_create_rectangle_equals(self):
+        r1 = Rectangle(3, 5, 1, 2, 7)
+        r1_dictionary = r1.to_dictionary()
+        r2 = Rectangle.create(**r1_dictionary)
+        self.assertNotEqual(r1, r2)
+
+    def test_create_rectangle_original(self):
+        r1 = Rectangle(3, 5, 1, 2, 7)
+        r1_dictionary = r1.to_dictionary()
+        r2 = Rectangle.create(**r1_dictionary)
+        self.assertEqual("[Rectangle] (7) 1/2 - 3/5", str(r1))
+
+    def test_create_rectangle_new(self):
+        r1 = Rectangle(3, 5, 1, 2, 7)
+        r1_dictionary = r1.to_dictionary()
+        r2 = Rectangle.create(**r1_dictionary)
+        self.assertEqual("[Rectangle] (7) 1/2 - 3/5", str(r2))
+
+    def test_create_square_equals(self):
+        s = Square(10)
+        s_dictionary = s.to_dictionary()
+        s1 = Square.create(**s_dictionary)
+        self.assertNotEqual(s, s1)
+
+
+class testBase_load_from_file(unittest.TestCase):
+    """
+    Tests for class method def load_from_file(cls):
+    that returns a list of instances:
     """
     pass
