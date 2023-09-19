0b0aab41b57f8455a946a4dfc902807628047571
Jukunye <jukunyes@gmail.com>
2023-09-19 05:49:38 +0300

    CSV

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,6 +3,7 @@
 """
 import json
 import os
+import csv
 
 
 class Base:
@@ -87,3 +88,36 @@ class Base:
         list_dict = cls.from_json_string(list_str)
 
         return [cls.create(**d) for d in list_dict]
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """Serializes in CVS"""
+        filename = cls.__name__ + ".csv"
+        with open(filename, mode="w", newline="") as file:
+            writer = csv.writer(file)
+            for obj in list_objs:
+                if cls.__name__ == "Rectangle":
+                    writer.writerow(
+                        [obj.id, obj.width, obj.height, obj.x, obj.y])
+                elif cls.__name__ == "Square":
+                    writer.writerow([obj.id, obj.size, obj.x, obj.y])
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """Deserializes in CSV"""
+        filename = cls.__name__ + ".csv"
+        try:
+            with open(filename, mode="r", newline="") as file:
+                reader = csv.reader(file)
+                obj_list = []
+                for row in reader:
+                    if cls.__name__ == "Rectangle":
+                        obj = cls(int(row[1]), int(row[2]),
+                                  int(row[3]), int(row[4]), int(row[0]))
+                    elif cls.__name__ == "Square":
+                        obj = cls(int(row[1]), int(row[2]),
+                                  int(row[3]), int(row[0]))
+                    obj_list.append(obj)
+                return obj_list
+        except FileNotFoundError:
+            return []
