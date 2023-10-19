e25e34ac0030c34747f1241c8cca57ce1d6c879f
Jukunye <jukunyes@gmail.com>
2023-10-19 04:27:19 +0300

    used backticks cities table

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -3,7 +3,7 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS `cities` (
     `id` INT NOT NULL AUTO_INCREMENT,
     `state_id` NOT NULL,
-    PRIMARY KEY (id),
+    PRIMARY KEY (`id`),
     FOREIGN KEY (`state_id`),
     REFERENCES `hbtn_0d_usa`.`states`(`id`)
 );
