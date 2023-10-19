f97bdaa8cf4e02c8538e6a144119024da3a1c1d7
Jukunye <jukunyes@gmail.com>
2023-10-19 04:30:35 +0300

    create table cities

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,6 +1,6 @@
 -- script that creates a database and table.
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS `cities` (
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities` (
     `id` INT NOT NULL AUTO_INCREMENT,
     `state_id` NOT NULL,
     PRIMARY KEY (`id`),
