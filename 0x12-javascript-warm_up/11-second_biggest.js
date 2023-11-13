54d4b8b92ed81f8caec3704cc784e229f326d274
Jukunye <jukunyes@gmail.com>
2023-11-13 18:33:00 +0300

    second biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,13 +1,8 @@
 #!/usr/bin/node
 const { argv } = require('process');
-let biggest = 0;
-let second = 0;
-
-for (let i = 2; i < argv.length; i++) {
-  if (parseInt(argv[i]) > biggest) {
-    second = biggest;
-    biggest = parseInt(argv[i]);
-  }
+if (argv.length <= 2) {
+    console.log(0);
+} else {
+    const arguments = argv.slice(2).map(Number).sort((a, b) => b - a)
+    console.log(arguments[1]);
 }
-
-console.log(second);
