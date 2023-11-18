9d8e444556ccfa93eb8d797c92ee8859f1603c05
Jukunye <jukunyes@gmail.com>
2023-11-18 10:24:07 +0300

    safe filter

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+This script that takes in an argument and displays all values
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    """
+    Access to the database and get the states
+    from the database.
+    """
+    connection = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
+    cursor = connection.cursor()
+
+    query = "SELECT * FROM states \
+            WHERE name LIKE BINARY %s \
+            ORDER BY states.id ASC;"
+
+    cursor.execute(query, (sys.argv[4],))
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    connection.close()
