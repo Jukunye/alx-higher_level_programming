8e8fb23e52233385f197ecb40b54a35984e11edb
Jukunye <jukunyes@gmail.com>
2023-12-01 15:44:20 +0300

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-# script that fetches https://alx-intranet.hbtn.io/status
+"""
+script that fetches https://alx-intranet.hbtn.io/status
+"""
 
 from urllib.request import urlopen
 
 if __name__ == "__main__":
     with urlopen('https://alx-intranet.hbtn.io/status') as response:
         html = response.read()
-        print('Body response:')
-        print(f"\t- type: {type(html)}")
-        print(f"\t- content: {html}")
-        print(f"\t- utf8 content: {html.decode(encoding='utf-8')}")
+        print("Body response:")
+    print("\t- type: {}".format(type(html)))
+    print("\t- content: {}".format(html))
+    print("\t- utf8 content: {}".format(html.decode(encoding='utf-8')))
